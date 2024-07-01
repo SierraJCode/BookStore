0ad39136f75fc2ce8156ac6a9b9ceda4bd1902dd
Jesus Sierra <sierraja06@gmail.com>
2024-07-01 00:22:33 -0500

    Todos los metodos funcionando en Controller

--- src/books/books.controller.ts
+++ src/books/books.controller.ts
@@ -1,4 +1,36 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post, Put } from '@nestjs/common';
+import { BooksService } from './books.service';
+import { Book } from './interface/book.interface';
+import { CreateBookDTO } from './dto/create-book.dto';
 
 @Controller('books')
-export class BooksController {}
+export class BooksController {
+    
+    constructor(private readonly booksService: BooksService){}
+
+    @Get()
+    async findAll():Promise<Book[]>{
+        return this.booksService.findAll();
+    };
+
+    @Get(':id')
+    async findOne(@Param('id') id: string): Promise<Book>{
+        return this.booksService.findOne(id);
+    };
+
+    @Post()
+    async create(@Body() createBookDTO: CreateBookDTO): Promise<Book>{
+        return this.booksService.create(createBookDTO);
+    };
+
+    @Put(':id')
+    async update(@Param(':id') id: string, @Body() createBookDTO: CreateBookDTO): Promise<Book>{
+        return this.booksService.update(id, createBookDTO);
+    };
+
+    @Delete(':id')
+    async delete(@Param('id') id: string){
+        return this.booksService.delete(id);
+    };
+
+}
