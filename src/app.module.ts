5f5d56663341a67c3209adab4113d4ac740a086b
Jesus Sierra <sierraja06@gmail.com>
2024-07-01 18:03:30 -0500

    Se creó Modulo, Service y Controller Comics

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,11 +3,13 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { BooksModule } from './books/books.module';
+import { ComicsModule } from './comics/comics.module';
 
 @Module({
   imports: [
     MongooseModule.forRoot('mongodb://localhost/nest-book-store'),
-    BooksModule
+    BooksModule,
+    ComicsModule
   ],
   controllers: [AppController],
   providers: [AppService],
---
+++ src/comics/comics.controller.ts
@@ -0,0 +1,4 @@
+import { Controller } from '@nestjs/common';
+
+@Controller('comics')
+export class ComicsController {}
