b4a67a313a0221c02b36b4d21778f4b4fb7316e7
Jesus Sierra <sierraja06@gmail.com>
2024-06-30 23:53:32 -0500

    Metodos findAll, FindOne, create.. creados

--- src/books/books.service.ts
+++ src/books/books.service.ts
@@ -1,4 +1,24 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { Book } from './interface/book.interface';
+import { CreateBookDTO } from './dto/create-book.dto';
 
 @Injectable()
-export class BooksService {}
+export class BooksService {
+
+    constructor(@InjectModel('Book') private readonly BookModel: Model<Book>){}
+
+    async findAll(): Promise<Book[]>{
+        return this.BookModel.find().exec();
+    };
+
+    async findOne(bookID: string): Promise<Book>{
+        return this.BookModel.findById(bookID).exec();
+    };
+
+    async create(createBookDTO:CreateBookDTO): Promise<Book>{
+        const createdBook = new this.BookModel(createBookDTO)
+        return createdBook;
+    }
+}
