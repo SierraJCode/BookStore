4a3618824b133fe302796bf6da85e1832137a67c
Jesus Sierra <sierraja06@gmail.com>
2024-07-01 20:34:57 -0500

    Controller Comics esta listo

--- src/comics/comics.controller.ts
+++ src/comics/comics.controller.ts
@@ -1,4 +1,36 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post, Put } from '@nestjs/common';
+import { ComicsService } from './comics.service';
+import { CreateComicDTO } from './dto/comic.dto';
+import { Comic } from './interfaces/comic.interface';
+import { CreateBookDTO } from 'src/books/dto/create-book.dto';
 
 @Controller('comics')
-export class ComicsController {}
+export class ComicsController {
+
+    constructor(private readonly comicsService: ComicsService){}
+
+    @Get()
+    async findAll(): Promise<Comic[]>{
+        return this.comicsService.findAll();
+    };
+
+    @Get(':id')
+    async findOne(@Param('id') id: string): Promise<Comic>{
+        return this.comicsService.findOne(id);
+    }
+
+    @Post()
+    async create(@Body() createComicDTO: CreateComicDTO): Promise<Comic>{
+        return this.comicsService.add(createComicDTO);
+    }
+
+    @Put('id')
+    async update(@Param('id') id: string, @Body() createBookDTO:CreateComicDTO){
+        return this.comicsService.update(id, createBookDTO);
+    }
+
+    @Delete('id')
+    async delete(@Param('id') id: string){
+        return this.comicsService.delete(id);
+    }
+}
