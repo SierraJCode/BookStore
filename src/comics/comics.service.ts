48b69d06be35ce526affe536d9b28e63291bc6a0
Jesus Sierra <sierraja06@gmail.com>
2024-07-01 20:04:14 -0500

    Service de comic listo

--- src/comics/comics.service.ts
+++ src/comics/comics.service.ts
@@ -1,4 +1,33 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Comic } from './interfaces/comic.interface';
+import { Model } from 'mongoose';
+import { CreateComicDTO } from './dto/comic.dto';
+
 
 @Injectable()
-export class ComicsService {}
+export class ComicsService {
+
+    constructor(@InjectModel('Comic')  private readonly comicModel: Model<Comic>){}
+
+    async findAll(){
+        return this.comicModel.find().exec();
+    };
+
+    async findOne(id: string){
+        return this.comicModel.findById(id).exec();
+    };
+
+    async add(createComicDTO: CreateComicDTO){
+        return this.comicModel.create(createComicDTO);
+    };
+
+    async uptadte(id: string, createComicDTO: CreateComicDTO){
+        return this.comicModel.findByIdAndUpdate(id, createComicDTO, {new: true}).exec();
+    };
+
+    async delete(id: string){
+        return this.comicModel.findByIdAndDelete(id).exec();
+    };
+
+}
