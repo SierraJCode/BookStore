48afdafb0d854436963ccc15cc2e75f9eb808331
Jesus Sierra <sierraja06@gmail.com>
2024-07-01 20:03:38 -0500

    Schema creado en module

--- src/comics/comics.module.ts
+++ src/comics/comics.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { ComicsService } from './comics.service';
 import { ComicsController } from './comics.controller';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ComicSchema } from './schema/comic.schema';
 
 @Module({
+  imports: [MongooseModule.forFeature([{name: 'Comic', schema: ComicSchema}])],
   providers: [ComicsService],
   controllers: [ComicsController]
 })
